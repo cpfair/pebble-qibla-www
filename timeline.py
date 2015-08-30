5c1c331907a86e607a5f7b453eefeb778569f2d8
Collin Fair <cpf@cpfx.ca>
2015-08-30 09:47:07 -0400

    Wipe old pins when re-pushing

--- timeline.py
+++ timeline.py
@@ -16,13 +16,17 @@ class Timeline:
         "isha": "Isha"
     }
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=20)
-    def push_pins_for_user(user, sync=False):
+    def push_pins_for_user(user, sync=False, clear=True):
         if not user.timeline_token:
             # They're not timeline-enabled
             return []
+        pending_pins = []
+
+        if clear:
+            for x in range(-2, 3):
+                pending_pins += Timeline._delete_pins_for_date(user, date.today() + timedelta(days=x))
         # Push pins for yesterday, today, tomorrow
         # (15s total)
-        pending_pins = []
         for x in range(-1, 2):
             pending_pins += Timeline._push_pins_for_date(user, date.today() + timedelta(days=x))
 
@@ -43,6 +47,21 @@ class Timeline:
         times = pt.getTimes(date, loc, 0, format="Float")
         for key in ["fajr", "sunrise", "dhuhr", "asr", "maghrib", "isha"]:
             yield Timeline.executor.submit(Timeline._push_time_pin, user, key, date, datetime.combine(date, time()).replace(tzinfo=pytz.utc) + timedelta(hours=times[key]))
+
+    def _delete_pins_for_date(user, date):
+        for key in ["fajr", "sunrise", "dhuhr", "asr", "maghrib", "isha"]:
+            yield Timeline.executor.submit(Timeline._delete_time_pin, user, key, date)
+
+    def _delete_time_pin(user, prayer, date):
+        pin_id = "%s:%s:%s" % (user.user_token, date, prayer)
+        res = requests.delete("https://timeline-api.getpebble.com/v1/user/pins/%s" % pin_id,
+                           headers={"X-User-Token": user.timeline_token, "Content-Type": "application/json"})
+        if res.status_code == 410:
+            # They've uninstalled the app
+            user.timeline_token = None
+            user.save()
+        assert res.status_code == 200, "Pin delete failed %s %s" % (res, res.text)
+        return True
 
     def _push_time_pin(user, prayer, date, timestamp):
         pin_data = Timeline._generate_pin(user, prayer, date, timestamp)
